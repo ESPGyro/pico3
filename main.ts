3366105d68fab6993da30514247f800a5fa86d58
ESP Gyro <99704174+ESPGyro@users.noreply.github.com>
2024-01-28 16:32:04 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,11 @@ let send_id = 255;
 //% color="#AA278D" icon="\uf2dc" weight=50
 namespace Picogame {
     //% block
-    //% blockId=sensor_read block="read sensor |%address"
+    //% blockId=sensor_read block="Receive data"
     //% address.min=1 address.max=5 address.defl=1
-    export function i2cRead(address: number): number {
+    export function i2cRead(): number {
 	let i2cbuf = pins.createBuffer(1);
-        i2cbuf[0] = send_id; 
+          i2cbuf[0] = send_id; 
         pins.i2cWriteBuffer(PG_ADDR, i2cbuf);
         let readbuf = pins.i2cReadBuffer(PG_ADDR, 1);
 	let receivedData = readbuf[0];
@@ -18,12 +18,11 @@ namespace Picogame {
 	}
 		return receivedData;
     }
-    //% blockId=sensor_write block="write value |%v"
+    //% blockId=sensor_write block="Broadcast value |%v"
     //% v.min=0 v.max=200
     export function i2cwrite(v: number): void {
 	let i2cbuf1 = pins.createBuffer(1);
         i2cbuf1[0] = v;
         pins.i2cWriteBuffer(PG_ADDR, i2cbuf1);
-    }
-	
+    }	
 }
