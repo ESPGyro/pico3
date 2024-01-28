7de8d9f481945d65d1315c957f44b3b06de2c76c
ESP Gyro <99704174+ESPGyro@users.noreply.github.com>
2024-01-28 15:31:35 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 const PG_ADDR = 0x66;
-let send_id = 255;
 //% color="#AA278D" icon="\uf2dc" weight=50
 namespace Picogame {
     //% block
@@ -11,19 +10,14 @@ namespace Picogame {
         pins.i2cWriteBuffer(PG_ADDR, i2cbuf);
         let readbuf = pins.i2cReadBuffer(PG_ADDR, 1);
 	let receivedData = readbuf[0];
-        if (receivedData !== undefined) {
-           send_id = readbuf[0];
-         } else {
-	   receivedData=0;
-       }    
         return receivedData;
     }
     //% blockId=sensor_write block="write value |%v"
     //% v.min=0 v.max=200
     export function i2cwrite(v: number): void {
-	let i2cbuf = pins.createBuffer(1);
-        i2cbuf[0] = v;
-        pins.i2cWriteBuffer(PG_ADDR, i2cbuf);
+	let i2cbuf1 = pins.createBuffer(1);
+        i2cbuf1[0] = v;
+        pins.i2cWriteBuffer(PG_ADDR, i2cbuf1);
     }
 	
 }
