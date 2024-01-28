36906ad6a974b3f19a468d54993fb80eec47cd3e
ESP Gyro <99704174+ESPGyro@users.noreply.github.com>
2024-01-28 08:44:17 +0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,33 @@
+const PG_ADDR = 0x66
+//% color="#AA278D" icon="\uf2dc" weight=50
+namespace Picogame {
+    //% block
+    //% blockId=sensor_read block="read sensor |%address"
+    //% address.min=1 address.max=5 address.defl=1
+    export function i2cRead(address: number): number {
+	let i2cbuf = pins.createBuffer(2)
+        i2cbuf[0] = 255
+        i2cbuf[1] = 255
+        pins.i2cWriteBuffer(PG_ADDR, i2cbuf)
+        let readbuf = pins.i2cReadBuffer(PG_ADDR, 1)
+        return readbuf[0]
+    }
+    //% blockId=sensor_write block="write value |%v"
+    //% v.min=0 v.max=200
+    export function i2cwrite(v: number): void {
+	let i2cbuf = pins.createBuffer(1)
+        i2cbuf[0] = v
+        pins.i2cWriteBuffer(PG_ADDR, i2cbuf)
+    }
+	
+	
+	type EvtMsg = (data: int) => void;
+	let datamsg: EvtMsg = null;
+	
+    //% block="On DATA received"
+    //% draggableParameters
+    //% weight=20
+    export function onDATAReceived(body: (receivedMessage: int) => void): void {
+        datamsg = body;
+    }
+}
